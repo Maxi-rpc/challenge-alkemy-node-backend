8bf20be53ce6ec667ac77c7ef30505ba4934ae26
Maximiliano <maxirpc2607@gmail.com>
2022-01-17 15:05:11 -0300

    Create node api

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application } from "express";
+
+// imp config
+
+// imp routes
+
+const app: Application = express();
+
+// config
+app.set("port", 4000);
+
+//routes
+//app.use(routes);
+
+export default app;
